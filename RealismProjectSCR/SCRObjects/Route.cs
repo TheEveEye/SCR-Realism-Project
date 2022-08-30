47eee8bd7b986be38ac8442572464b5489fc5771
TheEveEye <theeveeye@online.de>
2022-08-30 22:32:15 +0200

    1.10 Build 7

--- RealismProjectSCR/SCRObjects/Route.cs
+++ RealismProjectSCR/SCRObjects/Route.cs
@@ -6,17 +6,19 @@ using System.Threading.Tasks;
 using RealismProjectSCR.SCRObjects;
 using RealismProjectSCR.Units;
 using RealismProjectSCR.NetworkPlanner;
+using RealismProjectSCR;
 
 namespace RealismProjectSCR.SCRObjects
 {
     public class Route
     {
+        public static string RoutePath = Program.ProjectDirectoryPath + @"SCRObjects\Routes.SCR";
+
         public int RouteNumber { get; set; }
         public Station Terminus1 { get; set; }
         public Station Terminus2 { get; set; }
         public Station[] CallingStations { get; set; }
         public string Operator { get; set; }
-        public string RoutePath { get; set; }
         public string RouteName { get; set; }
 
         public Route(int RouteNumber, Station Terminus1, Station Terminus2, Station[] CallingStations, string Operator, string RouteName)
@@ -26,15 +28,18 @@ namespace RealismProjectSCR.SCRObjects
             this.Terminus2 = Terminus2;
             this.CallingStations = CallingStations;
             this.Operator = Operator;
-            this.RoutePath = Program.ProjectDirectoryPath + @"SCRObjects\Routes\" + this.RouteNumber + ".route";
             this.RouteName = RouteName;
         }
-    }
-    public struct Operator
-    {
-        public static int Connect = 0;
-        public static int Express = 1;
-        public static int Waterline = 2;
-        public static int Airlink = 3;
+        public static Route[] Import() // Requieres Timing class to be finished.
+        {
+            string[] file = File.ReadAllLines(RoutePath);
+            Route[] routes = new Route[file.Length];
+
+            for (int i = 0; i < routes.Length; i++)
+            {
+                string[] tempRoute = file[i].Split(';');
+
+            }
+        }
     }
 }
---
+++ RealismProjectSCR/SCRObjects/TimeTables/Timing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RealismProjectSCR;
+using RealismProjectSCR.Units;
+using RealismProjectSCR.SCRObjects.TimeTables;
+using RealismProjectSCR.SCRObjects;
+
+namespace RealismProjectSCR.SCRObjects.TimeTables
+{
+    public class Timing // This class is not done yet.
+    {
+        public static int 
+    }
+}
