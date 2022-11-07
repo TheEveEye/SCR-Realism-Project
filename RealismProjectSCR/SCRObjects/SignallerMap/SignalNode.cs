ee2c40cc6aec30a3e628a34dc44b91d579231f68
Eve <98696632+TheEveEye@users.noreply.github.com>
2022-11-07 12:44:52 +0100

    Structure
    Added variables and enum for the signal node class

---
+++ RealismProjectSCR/SCRObjects/SignallerMap/SignalNode.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RealismProjectSCR;
+using RealismProjectSCR.SCRObjects;
+using RealismProjectSCR.Units;
+using RealismProjectSCR.SCRObjects.TimeTables;
+using RealismProjectSCR.SCRObjects.SignallerMap;
+
+namespace RealismProjectSCR.SCRObjects.SignallerMap
+{
+    public class SignalNode
+    {
+        public Type SignalType { get; set; }
+        public Aspect SignalAspect { get; set; }
+        public int ID { get; set; }
+        public Segment[] AdjacentSegments { get; set; }
+        
+        enum Type
+        {
+            4Aspect = 0,
+            3Aspect = 1,
+            2Aspect = 2,
+            Shunt = 3
+        }
+        enum Aspect
+        {
+            Proceed = 0,
+            PreliminaryCaution = 1,
+            Caution = 2,
+            Danger = 3
+        }
+        
+        public Signal(Type SignalType, Aspect SignalAspect, int ID, Segment[] AdjacentSegments)
+        {
+            this.SignalType = SignalType;
+            this.SignalAspect = SignalAspect;
+            this.ID = ID;
+            this.AdjacentSegments = AdjacentSegments;
+        }
+    }
+}
