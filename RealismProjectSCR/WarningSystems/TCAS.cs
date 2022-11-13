1e439fb4790a9f3468f950d8a6624e8b6c939d2c
TheEveEye <theeveeye@online.de>
2022-11-13 21:38:07 +0100

    Added SameStation and SamePlatforms

--- RealismProjectSCR/WarningSystems/TCAS.cs
+++ RealismProjectSCR/WarningSystems/TCAS.cs
@@ -14,6 +14,14 @@ namespace RealismProjectSCR
 
         static string ThresholdWarning01 = "";
 
+        public enum CollisionStatus
+        {
+            SameStation = 1,
+            SamePlatform = 2,
+            SameNextStation = 3,
+            SameStationExit = 4,
+            SameStationEntrance = 5,
+        }
 
         public static bool DepartureCollision(List<Departure> departures, Departure newDeparture)
         {
@@ -43,6 +51,27 @@ namespace RealismProjectSCR
                 }
             }
             return output;
+        }
+        public static List<CollisionStatus> GetCollisionStatus(Departure departure1, Departure departure2)
+        {
+            List<CollisionStatus> collisionStatuses = new List<CollisionStatus>();
+
+            if ((departure1.Station.Equals(departure2.Station)) && (TimeFrame.Conflicts(departure1.Occupation, departure2.Occupation)))
+            {
+                collisionStatuses.Add(CollisionStatus.SameStation);
+            }
+            for (int i = 0; i < departure2.PossiblePlatforms.Length; i++)
+            {
+                if ((departure1.PossiblePlatforms.Contains(departure2.PossiblePlatforms[i])) && (TimeFrame.Conflicts(departure1.Occupation, departure2.Occupation)) && (!collisionStatuses.Contains(CollisionStatus.SamePlatform)))
+                {
+                    collisionStatuses.Add(CollisionStatus.SamePlatform);
+                }
+            }
+            // Add CollisionStatus.SameNextStation
+            // Add CollisionStatus.SameStationEntrance
+            // Add CollisionStatus.SameStationExit
+
+            return collisionStatuses;
         }
     }
 }
